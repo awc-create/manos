c415f0b2742d7dae5f5f4936b4d22b76675d75d2
Adnan 2025 <adaptiveworkflowconsultancy@gmail.com>
2026-03-17 04:33:01 +0000

    first itteration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
+// site/next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   trailingSlash: true,
-  images: { domains: ['cdn.example.com'] },
+  eslint: { ignoreDuringBuilds: true },
+
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      // UploadThing / utfs
+      { protocol: 'https', hostname: 'utfs.io', pathname: '/**' },
+      { protocol: 'https', hostname: 'ssl.gstatic.com', pathname: '/**' },
+      { protocol: 'https', hostname: '**.cdninstagram.com', pathname: '/**' },
+      { protocol: 'https', hostname: '**.fbcdn.net', pathname: '/**' },
+      { protocol: 'https', hostname: 'scontent.cdninstagram.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'prince-foods-media.hel1.your-objectstorage.com' },
+    ],
+  },
+
+  outputFileTracingIncludes: {
+    '/**/*': ['./node_modules/bcryptjs/**'],
+  },
 };
+
 module.exports = nextConfig;
